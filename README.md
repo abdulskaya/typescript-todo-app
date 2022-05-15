8788e714bafdd151b34ddbf1cfda47f5e95dd71a
Abdulsamet KAYA <abdulskaya@gmail.com>
2022-05-16 01:02:36 +0300

    add tasking system

--- README.md
+++ README.md
@@ -22,3 +22,10 @@ Your app is ready to be deployed!
 ### Q: What about Eject?
 
 No eject needed! Snowpack guarantees zero lock-in, and CSA strives for the same.
+
+
+### Create a same this template with snowpack
+
+``
+npx create-snowpack-app . --template @snowpack/app-template-blank-typescript --force
+``
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,65 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid'
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: string,
+  title: string,
+  completed: boolean,
+  createdAt: Date
+}
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>('#list')
+const form = document.getElementById('new-task-form') as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>('#new-task-title')
+const tasks: Task[] = loadTasks()
+
+tasks.forEach(addListItem)
+
+form?.addEventListener('submit', e => {
+  e.preventDefault()
+  
+  if(input?.value == '' || input?.value == null ) return 
+  
+  const newTask: Task =  {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+
+  tasks.push(newTask)
+  saveTasks()
+
+  addListItem(newTask)
+  input.value = ''
+  
+})
+
+function addListItem(task: Task) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox'
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+}
+
+function saveTasks() {
+  console.log("tasks");
+  console.log(tasks);
+  console.log("tasks storage");
+  console.log(loadTasks());
+  
+  localStorage.setItem('TASKS', JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem('TASKS')
+  if(taskJSON == null) return []
+  return JSON.parse(taskJSON)
+}
